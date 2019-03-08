38060ee0f08bf94b58028ed096876b74bffffce4
Lafe1 <43574247+Lafe1@users.noreply.github.com>
2019-03-08 20:31:48 +0300

    beta

--- Gas_station/Form1.Designer.cs
+++ Gas_station/Form1.Designer.cs
@@ -28,13 +28,277 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sum1000 = new System.Windows.Forms.Label();
+            this.sum50 = new System.Windows.Forms.Label();
+            this.sum100 = new System.Windows.Forms.Label();
+            this.sum500 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-188, -382);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1455, 1318);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(12, 23);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(300, 550);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.GhostWhite;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(48, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 125);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Внесите сумму\r\nТерминал принимает\r\nкупюры 50, 100, 500, \r\n1000 рублей\r\nСдачу не в" +
+    "ыдает!\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(120, 255);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(82, 25);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(318, 23);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(156, 257);
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(327, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 50);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Какую сумму\r\n вносите?\r\n";
+            this.label2.Visible = false;
+            // 
+            // sum1000
+            // 
+            this.sum1000.AutoSize = true;
+            this.sum1000.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sum1000.Location = new System.Drawing.Point(327, 102);
+            this.sum1000.Name = "sum1000";
+            this.sum1000.Size = new System.Drawing.Size(67, 25);
+            this.sum1000.TabIndex = 8;
+            this.sum1000.Text = "1000р";
+            this.sum1000.Visible = false;
+            this.sum1000.Click += new System.EventHandler(this.sum1000_Click);
+            // 
+            // sum50
+            // 
+            this.sum50.AutoSize = true;
+            this.sum50.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sum50.Location = new System.Drawing.Point(327, 222);
+            this.sum50.Name = "sum50";
+            this.sum50.Size = new System.Drawing.Size(45, 25);
+            this.sum50.TabIndex = 9;
+            this.sum50.Text = "50р";
+            this.sum50.Visible = false;
+            this.sum50.Click += new System.EventHandler(this.sum50_Click);
+            // 
+            // sum100
+            // 
+            this.sum100.AutoSize = true;
+            this.sum100.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sum100.Location = new System.Drawing.Point(327, 183);
+            this.sum100.Name = "sum100";
+            this.sum100.Size = new System.Drawing.Size(56, 25);
+            this.sum100.TabIndex = 10;
+            this.sum100.Text = "100р";
+            this.sum100.Visible = false;
+            this.sum100.Click += new System.EventHandler(this.sum100_Click);
+            // 
+            // sum500
+            // 
+            this.sum500.AutoSize = true;
+            this.sum500.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sum500.Location = new System.Drawing.Point(327, 143);
+            this.sum500.Name = "sum500";
+            this.sum500.Size = new System.Drawing.Size(56, 25);
+            this.sum500.TabIndex = 11;
+            this.sum500.Text = "500р";
+            this.sum500.Visible = false;
+            this.sum500.Click += new System.EventHandler(this.sum500_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(204, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 25);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Готово";
+            this.label3.Visible = false;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(39, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 25);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Отмена";
+            this.label4.Visible = false;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(39, 143);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 50);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "АИ-92\r\n40р\\л";
+            this.label5.Visible = false;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(215, 143);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 50);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "АИ-95\r\n45р\\л";
+            this.label6.Visible = false;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(353, 360);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(209, 253);
+            this.pictureBox5.TabIndex = 17;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(384, 398);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(42, 175);
+            this.pictureBox6.TabIndex = 18;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click_1);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox7.Image")));
+            this.pictureBox7.Location = new System.Drawing.Point(484, 398);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(39, 175);
+            this.pictureBox7.TabIndex = 19;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(985, 568);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.sum500);
+            this.Controls.Add(this.sum100);
+            this.Controls.Add(this.sum50);
+            this.Controls.Add(this.sum1000);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label sum1000;
+        private System.Windows.Forms.Label sum50;
+        private System.Windows.Forms.Label sum100;
+        private System.Windows.Forms.Label sum500;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
     }
 }
 
--- Gas_station/Form1.cs
+++ Gas_station/Form1.cs
@@ -12,9 +12,225 @@ namespace Gas_station
 {
     public partial class Form1 : Form
     {
+        bool flooded92 = false;
+        bool flooded95 = false;
+        bool paid92 = false;
+        bool paid95 = false;
+        int sum = 0;
+
         public Form1()
         {
             InitializeComponent();
+            pictureBox6.BringToFront();
+            pictureBox7.BringToFront();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            pictureBox4.Visible = true;
+            label2.BringToFront();
+            label4.BringToFront();
+            sum50.BringToFront();
+            sum100.BringToFront();
+            sum1000.BringToFront();
+            sum500.BringToFront();
+            label2.Visible = true;
+            sum50.Visible = true;
+            sum100.Visible = true;
+            sum1000.Visible = true;
+            sum500.Visible = true;
+            label4.Visible = true;
+            pictureBox3.SendToBack();
+            pictureBox3.Visible = false;
+
+
+        }
+
+        private void sum1000_Click(object sender, EventArgs e)
+        {
+            sum += 1000;
+            label1.Text = "Нажмите 'готово', \n чтобы прекратить\n внесение. \nВы внесли: " + sum + "р";
+            label3.BringToFront();
+            label3.Visible = true;
+
+        }
+
+        private void sum500_Click(object sender, EventArgs e)
+        {
+            sum += 500;
+            label1.Text = "Нажмите 'готово', \n чтобы закончить\n внесение. \nВы внесли: " + sum + "р";
+            label3.BringToFront();
+            label3.Visible = true;
+        }
+
+        private void sum100_Click(object sender, EventArgs e)
+        {
+            sum += 100;
+            label1.Text = "Нажмите 'готово', \n чтобы закончить\n внесение. \nВы внесли: " + sum + "р";
+            label3.BringToFront();
+            label3.Visible = true;
+        }
+
+        private void sum50_Click(object sender, EventArgs e)
+        {
+            sum += 50;
+            label1.Text = "Нажмите 'готово', \n чтобы закончить\n внесение. \nВы внесли: " + sum + "р";
+            label3.BringToFront();
+            label3.Visible = true;
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            pictureBox4.Visible = false;
+            label2.SendToBack();
+            sum50.SendToBack();
+            sum100.SendToBack();
+            sum1000.SendToBack();
+            sum500.SendToBack();
+            label2.Visible = false;
+            sum50.Visible = false;
+            sum100.Visible = false;
+            sum1000.Visible = false;
+            sum500.Visible = false;
+            label3.SendToBack();
+            label3.Visible = false;
+            label4.Visible = false;
+            label1.Text = "Внесите сумму\nТерминал принимает\nкупюры 50, 100, 500,\n1000 рублей\nСдачу не выдает!";
+            pictureBox3.BringToFront();
+            pictureBox3.Visible = true;
+            paid92 = false;
+            sum = 0;
+
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            pictureBox4.Visible = false;
+            label2.SendToBack();
+            sum50.SendToBack();
+            sum100.SendToBack();
+            sum1000.SendToBack();
+            sum500.SendToBack();
+            label2.Visible = false;
+            sum50.Visible = false;
+            sum100.Visible = false;
+            sum1000.Visible = false;
+            sum500.Visible = false;
+            label3.SendToBack();
+            label3.Visible = false;
+            label4.Visible = false;
+            label1.Text = "Вы внесли: " + sum + "р\nВыберите вид\nбензина:";
+            label5.BringToFront();
+            label5.Visible = true;
+            label6.BringToFront();
+            label6.Visible = true;
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            int quantity = sum / 40;
+            label1.Text = "Будет выдано: " + quantity + "л\nВозьмите:\nшланг 92";
+            label5.Visible = false;
+            label6.Visible = false;
+            label4.Visible = true;
+            paid92 = true;
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            int quantity = sum / 45;
+            label1.Text = "Будет выдано: " + quantity + "л\nВозьмите:\nшланг 95";
+            label5.Visible = false;
+            label6.Visible = false;
+            label4.Visible = true;
+            paid95 = true;
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void pictureBox5_Click(object sender, EventArgs e)
+        {
+            if (flooded92)
+            {
+                flooded92 = false;
+                pictureBox6.Location = new Point(384, 450);
+                await Task.Delay(500);
+                pictureBox6.Location = new Point(384, 398);
+                label1.Text = "Счастливого пути!";
+                await Task.Delay(3000);
+                label1.Text = "Внесите сумму\nТерминал принимает\nкупюры 50, 100, 500,\n1000 рублей\nСдачу не выдает!";
+                pictureBox3.Visible = true;
+                pictureBox3.BringToFront();
+            }
+            if (flooded95)
+            {
+                flooded95 = false;
+                pictureBox7.Location = new Point(484, 450);
+                await Task.Delay(500);
+                pictureBox7.Location = new Point(484, 398);
+                label1.Text = "Счастливого пути!";
+                await Task.Delay(3000);
+                label1.Text = "Внесите сумму\nТерминал принимает\nкупюры 50, 100, 500,\n1000 рублей\nСдачу не выдает!";
+                pictureBox3.Visible = true;
+                pictureBox3.BringToFront();
+            }
+        }
+
+        private async void pictureBox6_Click_1(object sender, EventArgs e)
+        {
+
+            if (paid92)
+            {
+                label4.Visible = false;
+                pictureBox6.Location = new Point(384, 450);
+                await Task.Delay(500);
+                pictureBox6.Location = new Point(384, 500);
+                await Task.Delay(500);
+                pictureBox6.Location = new Point(384, 600);
+                await Task.Delay(500);
+                pictureBox6.Location = new Point(384, 700);
+                label1.Text = "Подождите, бак\n наполняется!";
+                await Task.Delay(3000);
+                label1.Text = "Бак наполнен!\nВерните шланг\nна место";
+                sum = 0;
+                flooded92 = true;
+                paid92 = false;
+
+            }
+
+
+        }
+
+        private async void pictureBox7_Click(object sender, EventArgs e)
+        {
+            if (paid95)
+            {
+                label4.Visible = false;
+                pictureBox7.Location = new Point(484, 450);
+                await Task.Delay(500);
+                pictureBox7.Location = new Point(484, 500);
+                await Task.Delay(500);
+                pictureBox7.Location = new Point(484, 600);
+                await Task.Delay(500);
+                pictureBox7.Location = new Point(484, 700);
+                label1.Text = "Подождите, бак\n наполняется!";
+                await Task.Delay(3000);
+                label1.Text = "Бак наполнен!\nВерните шланг\nна место";
+                sum = 0;
+                flooded95 = true;
+                paid95 = false;
+
+            }
         }
     }
 }
